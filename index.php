4b82d4d8f0578fa6c4ef6e9de8f4c948b7480c7f
OcimaOcram <marco.amico.pesci00@gmail.com>
2022-07-07 16:53:04 +0200

    Esercizio completato

--- index.php
+++ index.php
@@ -1,8 +1,43 @@
 <?php
-include DIR . '/movie.php';
+include __DIR__ . '/Movie.php';
 
-$shrek = new Movie();
-$shrek-> titolo = "Shrek";
-$shrek-> regista = "Vicky Jenson";
+$shrek = new Movie('Shrek', 'Viky', 10);
 
-var_dump($shrek);
+
+$titanic = new Movie('Titanic', 'James Cameron', 15 );
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h3>
+        Titolo: <?= $shrek->titolo ?>
+    </h3>
+    <div>
+        Regista: <?= $shrek->regista ?>
+    </div>
+    <div>
+        prezzo: <?= $shrek->prezzo ?>
+    </div>
+
+    <h3>
+        Titolo: <?= $titanic->titolo ?>
+    </h3>
+    <div>
+        Regista: <?= $titanic->regista ?>
+    </div>
+    <div>
+        prezzo: <?= $titanic->prezzo ?>
+    </div>
+    
+</body>
+</html>
--- movie.php
+++ movie.php
@@ -3,4 +3,20 @@
 class Movie {
     public $titolo;
     public $regista;
+    public $prezzo;
+    public $sconto;
+
+
+    public function __construct($_titolo, $_regista, $_prezzo)
+    {
+        $this->titolo = $_titolo;
+        $this->regista = $_regista;
+        $this->prezzo = $_prezzo;
+        $this->sconto();
+    }
+    public function sconto() {
+        if ($this->prezzo >= 10)
+        $this->prezzo = 8;
+
+    }
 }
